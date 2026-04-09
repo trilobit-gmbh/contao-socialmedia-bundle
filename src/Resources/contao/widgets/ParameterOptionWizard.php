16df8bf2d1c620b446ffa52fcd6fe41ad12bcd2f
Peter Adelmann <peter.adelmann@trilobit.de>
2026-04-09 15:26:33 +0200

    adjust contao 5.7 compatibility

--- src/Resources/contao/widgets/ParameterOptionWizard.php
+++ src/Resources/contao/widgets/ParameterOptionWizard.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace Trilobit\SocialmediaBundle;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\Image;
 use Contao\StringUtil;
 use Contao\Widget;
@@ -84,48 +85,50 @@ class ParameterOptionWizard extends Widget
      */
     public function generate()
     {
-        $arrButtons = ['copy', 'delete', 'drag'];
-
         // Make sure there is at least an empty array
         if (!\is_array($this->varValue) || !isset($this->varValue[0])) {
             $this->varValue = [['']];
         }
 
+        $version = (method_exists(ContaoCoreBundle::class, 'getVersion') ? ContaoCoreBundle::getVersion() : VERSION);
+
         // Begin the table
-        $return = '<table id="ctrl_'.$this->strId.'" class="tl_optionwizard">
+        $buffer = '<table id="ctrl_'.$this->strId.'" class="tl_optionwizard">
   <thead>
     <tr>
+      '.(version_compare($version, '5.7', '<')
+            ? ''
+            : '<th></th>'
+        ).'
+      <th>'.$GLOBALS['TL_LANG']['MSC']['ow_key'].'</th>
       <th>'.$GLOBALS['TL_LANG']['MSC']['ow_value'].'</th>
-      <th>'.$GLOBALS['TL_LANG']['MSC']['ow_label'].'</th>
       <th></th>
     </tr>
   </thead>
   <tbody class="sortable">';
 
         // Add fields
-        for ($i = 0, $c = \count($this->varValue); $i < $c; ++$i) {
-            $return .= '
-    <tr>
-      <td><input type="text" name="'.$this->strId.'['.$i.'][value]" id="'.$this->strId.'_value_'.$i.'" class="tl_text" value="'.StringUtil::specialchars($this->varValue[$i]['value'] ?? '').'"></td>
-      <td><input type="text" name="'.$this->strId.'['.$i.'][label]" id="'.$this->strId.'_label_'.$i.'" class="tl_text" value="'.StringUtil::specialchars($this->varValue[$i]['label'] ?? '').'"></td>';
-
-            // Add row buttons
-            $return .= '
-      <td>';
-
-            foreach ($arrButtons as $button) {
-                if ('drag' === $button) {
-                    $return .= ' <button type="button" class="drag-handle" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['move']).'">'.Image::getHtml('drag.svg').'</button>';
-                } else {
-                    $return .= ' <button type="button" data-command="'.$button.'" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['ow_'.$button]).'">'.Image::getHtml($button.'.svg').'</button>';
-                }
-            }
-
-            $return .= '</td>
-    </tr>';
+        foreach ($this->varValue as $key => $value) {
+            $buffer .= '
+<tr>
+    '.(version_compare($version, '5.7', '<')
+        ? ''
+        : '<td><button type="button" class="drag-handle" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['move']).'">'.Image::getHtml('drag.svg').'</button></td>'
+            ).'
+    <td><input type="text" name="'.$this->strId.'['.$key.'][value]" id="'.$this->strId.'_value_'.$key.'" class="tl_text" value="'.StringUtil::specialchars($value['value'] ?? '').'"></td>
+    <td><input type="text" name="'.$this->strId.'['.$key.'][label]" id="'.$this->strId.'_label_'.$key.'" class="tl_text" value="'.StringUtil::specialchars($value['label'] ?? '').'"></td>
+    <td>
+        <button type="button" data-command="copy" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['ow_copy']).'">'.Image::getHtml('copy.svg').'</button>
+        <button type="button" data-command="delete" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['ow_delete']).'">'.Image::getHtml('delete.svg').'</button>
+    '.(version_compare($version, '5.7', '<')
+                            ? '<button type="button" class="drag-handle" title="'.StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['move']).'">'.Image::getHtml('drag.svg').'</button>'
+                            : ''
+            ).'
+    </td>
+</tr>';
         }
 
-        return $return.'
+        return $buffer.'
   </tbody>
   </table>
   <script>Backend.optionsWizard("ctrl_'.$this->strId.'")</script>';
